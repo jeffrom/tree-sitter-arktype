52f707c58416643295a4c4b6353194a028a123f3
Jeff Martin <jeffmartin@gmail.com>
2025-11-08 10:36:59 -0800

    add some injection queries

---
+++ contrib/queries/ecma/injections.scm
@@ -0,0 +1,35 @@
+(call_expression
+  function: (identifier) @_name
+  (#any-of? @_name "type" "scope")
+  arguments: [(arguments
+    (string
+      (string_fragment) @injection.content))
+
+    (arguments
+      (object
+        (pair
+          key: ([(property_identifier) (string)]
+            value: (string
+              (string_fragment) @injection.content)))))
+
+    ; go a level deeper
+    (arguments
+      (object
+        (pair
+          key: ([(property_identifier) (string)]
+            value: (object
+              (pair
+               key: ([(property_identifier) (string)]
+             value: (string
+               (string_fragment) @injection.content))))))))]
+  (#set! injection.language "arktype"))
+
+(call_expression
+  function: (member_expression
+              object: (identifier) @_name
+              (#eq? @_name "type")
+              property: (property_identifier))
+  arguments: (arguments
+               (string
+                 (string_fragment) @injection.content
+                 (#set! injection.language "arktype"))))
